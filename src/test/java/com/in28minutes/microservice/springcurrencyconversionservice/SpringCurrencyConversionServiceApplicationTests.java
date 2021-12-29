f6bc4f439db75975733284cba0effa7c50378e38
shitalsadgir <sheetalsadgir@gmail.com>
2021-12-29 16:18:01 -0600

    initial commit

---
+++ src/test/java/com/in28minutes/microservice/springcurrencyconversionservice/SpringCurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservice.springcurrencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
